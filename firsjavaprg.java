9be06ffa721db409524c3ea877788e678c8b5590
kalyanbrp <kalyanbrp@gmail.com>
2022-12-12 05:34:26 +0530

    Update firsjavaprg.java

--- firsjavaprg.java
+++ firsjavaprg.java
@@ -2,7 +2,10 @@ class firstjavaprg
 {
 public static void main(String[] args)
 {
+  int a=10;
+  int b=20;
 System.out.println(" welcome to java programming");
+  System.out.println(" sum of the two values is "+(a+b));
 }
 }
 
